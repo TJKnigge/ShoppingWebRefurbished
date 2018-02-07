589693c01ef80d29647de8d5b01428d835ee2c61
unknown <you@example.com>
2018-02-07 11:09:47 +0100

    first commit

---
+++ General.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * 
+ * Made by T. J. Knigge.
+ * This project is to bring more structure in an old project
+ */
+
+session_start();
+
+
+
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Made by T. J. Knigge.
+This project is to bring more structure in an old project
+
+-->
+<?php
+require 'General.php';
+
+?>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
